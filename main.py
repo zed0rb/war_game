f9ef36287e9ce01f733b85074b103064eb5a0ee1
miglemr <migler019@gmail.com>
2023-08-01 22:17:56 +0300

    Defined main function

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import random
 
+
 class Deck:
     suits = ["♠", "♥", "♦", "♣"]
-    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    values = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+        "Ace",
+    ]
 
     def init(self):
         self.cards = []
@@ -14,9 +29,22 @@ class Deck:
                 self.cards.append(i + " " + j)
         return self.cards
 
-    #method to split the deck in 2
+    # method to split the deck in 2
     def deal(self):
         random.shuffle(self.cards)
         self.deck1 = self.cards[:26]
         self.deck2 = self.cards[26:]
         return self.deck1, self.deck2
+
+
+def main():
+    deck = Deck()
+    deck1, deck2 = deck.deal()
+
+    start = input(
+        "Game begins. To draw a card, type 'd', to finish the game, type 'q'. Start game by drawing a card: "
+    )
+
+
+if __name__ == "__main__":
+    main()
